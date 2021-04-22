54ec2bb99d17253277839ae837e92e848ebea604
Ahmed Roble <airoble2019@gmail.com>
2021-04-22 19:43:10 -0400

    Include responsive photo gallery & search box functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-const search = new Filter("search", "data-caption")
 baguetteBox.run(".gallery")
+
+function filterImages() {
+  // Declare variables
+  let input = document.getElementById("search").value.toLowerCase()
+  let x = document.getElementsByTagName("a")
+
+  // Loop through all the a elements and hide those who don't match the search query
+  for (let i = 0; i < x.length; i++) {
+    let imageCaption = x[i].getAttribute("data-caption").toLowerCase()
+    if (!imageCaption.includes(input)) {
+      x[i].style.display = "none"
+    } else {
+      x[i].style.display = "inline-block"
+    }
+  }
+}
